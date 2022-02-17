b37cf92a27e9ede72a975d02f5f143e76dea5638
MakarovVA <makmail@list.ru>
2022-02-17 20:30:24 +0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+class Main {
+    public static void main(String[] args) {
+        boolean isGreenLight = false;
+
+        int speedOfPlayer1 = 5;
+        int speedOfPlayer2 = 0;
+        int speedOfPlayer3 = 2;
+
+        // Допишите здесь логику так, что будет подсчитано и выведено
+        // количество игроков, которые выбывают.
+        // Если свет зелёный, то проходят все игроки (0 выбывают).
+        // Если свет красный, то выбывает каждый, чья скорость не 0.
+    }
+}
